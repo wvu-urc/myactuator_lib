4b7e6d2a3712044c18905384ad389dac6ea68385
nate-adkins <nathanpadkins@gmail.com>
2023-09-07 13:02:22 -0400

    made library a pip library

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myactuator_library",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A description of the myactuator_library.",
+    license="MIT",
+    keywords="actuator robotics",
+    url="http://example.com/myactuator_library",   # project homepage
+)
