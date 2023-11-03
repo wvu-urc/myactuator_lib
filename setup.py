40297f44d4b91bff3069a8c07bd72e2122d7f3cb
nate-adkins <nathanpadkins@gmail.com>
2023-11-02 22:18:02 -0400

    library to lib

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="myactuator_library",
-    version="0.1.0",
+    name="myactuator_lib",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
 
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A description of the myactuator_library.",
+    author="Nathan Adkins",
+    author_email="npa00003@mix.wvu.edu",
+    description="Defines classes for controlling myactuator motors.",
     license="MIT",
     keywords="actuator robotics",
-    url="http://example.com/myactuator_library",   # project homepage
+    url="https://github.com/wvu-urc/myactuator_lib",   # project homepage
 )
